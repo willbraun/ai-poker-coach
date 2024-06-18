27792b0603dc8250a323022009d862f5365b27e2
willbraun <williamhbraun1@gmail.com>
2024-06-18 08:39:55 -0400

    modified LoginResponseDto

--- Models/DataTransferObjects/Authentication/LoginResponseDto.cs
+++ Models/DataTransferObjects/Authentication/LoginResponseDto.cs
@@ -6,11 +6,15 @@ namespace ai_poker_coach.Models.DataTransferObjects
     {
         public string UserId { get; set; } = "";
         public string AccessToken { get; set; } = "";
+        public int ExpiresIn { get; set; } = 0;
+        public string RefreshToken { get; set; } = "";
 
         public LoginResponseDto(ApplicationUser user, LoginInnerResponseDto loginInnerResponseDto)
         {
             UserId = user.Id;
             AccessToken = loginInnerResponseDto.accessToken;
+            ExpiresIn = loginInnerResponseDto.expiresIn;
+            RefreshToken = loginInnerResponseDto.refreshToken;
         }
     }
 }
